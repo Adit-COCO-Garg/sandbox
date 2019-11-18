9d6b165876f215925c9e26108fb8fe7a0f38fbcf
Adit-COCO-Garg <adit.garg21k@gmail.com>
2019-11-18 17:08:13 -0500

    f

---
+++ Blobs.js
@@ -0,0 +1,10 @@
+'use strict';
+
+class Blobs{
+	// #blobType = "standard";
+	// #speed = 3;
+	constructor(speed, type){
+		this.speed = speed;
+		this.type = type;
+	}
+}
---
+++ sketch.js
@@ -0,0 +1,22 @@
+/**
+ * Adit Garg
+ * IGME-102: Project 2, m/d/18
+ * Summarization of sketch activity
+ */
+"use strict"; //catch some common coding errors
+
+function setup(){
+	canvasCreat(); //canvas creator function called
+	
+}
+function draw(){
+	
+}
+
+function windowResized() {
+	h = innerHeight
+	w = innerWidth
+	canvas = resizeCanvas(innerWidth, innerHeight);
+	//background(207, 211, 212); //Light Gray Background
+	//tractData.forEach(t => t.display())
+}
